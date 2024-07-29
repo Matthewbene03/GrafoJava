2d2b997112eb3dffa8eb50258d1683aaa3abef32
Matheus Benevenuto <matthewbene03@gmail.com>
2024-07-29 12:16:18 -0300

    Atualizações nas clessas do grafo - Granfo não ponderado e com direção.

--- src/app/App.java
+++ src/app/App.java
@@ -1,16 +1,17 @@
 package app;
 
 import app.lista.grafossempeso.Grafo;
-import app.lista.grafossempeso.GrafoNaoDirecionado;
+import app.lista.grafossempeso.GrafoDirecionado;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    	Grafo grafo = new GrafoNaoDirecionado(5);
+    	Grafo grafo = new GrafoDirecionado(5);
         grafo.insertAresta(0, 1);
-        grafo.insertAresta(0, 2);
         grafo.insertAresta(1, 2);
+        grafo.insertAresta(2, 0);
         grafo.insertAresta(2, 4);
-        grafo.insertAresta(4, 3);        
+        grafo.insertAresta(4, 3); 
+        grafo.insertAresta(3, 1);
 
         System.out.println("Grafo adicionado. Vamos imprimir: ");
         System.out.println("------------------------------------------------------------------------");
@@ -19,7 +20,7 @@ public class App {
 
 
         grafo.deletedAresta(0,1);
-        grafo.deletedAresta(2,0);
+        grafo.deletedAresta(0,2);
         System.out.println("Duas arestas removidas. Vamos imprimir: ");
         System.out.println("------------------------------------------------------------------------");
         grafo.imprimeGrafo();
